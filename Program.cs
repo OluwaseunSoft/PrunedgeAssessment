0335997a148c2140b17fbd8bc8161b7f63331368
Oluwaseun Akinwale <oluwaseunazure@yahoo.com>
2022-05-24 21:49:32 +0100

    creating platform

--- Program.cs
+++ Program.cs
@@ -15,14 +15,28 @@ namespace akinwaleoluwaseun2015_gmail.com
 
         public static int MinimumPlatformRequired(int[] arrival, int[] departure)
         {
+            int result = 1, platform = 1, i = 1, j = 0;
+
             try
             {
+                Array.Sort(arrival);
+                Array.Sort(departure);
 
+                while (i < arrival.Length && j < arrival.Length)
+                {
+                    if(arrival[i] <= departure[j])
+                    {
+                        platform++;
+                        i++;
+                    }
+                }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-
+                Console.WriteLine(ex.Message.ToString());
             }
+
+            return result;
         }
     }
 }
